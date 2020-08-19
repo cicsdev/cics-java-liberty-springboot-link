163f226f5e0f2d913e00b9b7279d173afa565de1
P. I. Wakelin <phil_wakelin@uk.ibm.com>
2020-08-19 10:19:25 +0100

    Update licenses

--- src/main/java/com/ibm/cicsdev/springboot/link/app/ui/ServletInitializer.java
+++ src/main/java/com/ibm/cicsdev/springboot/link/app/ui/ServletInitializer.java
@@ -1,3 +1,13 @@
+/* Licensed Materials - Property of IBM                                   */
+/*                                                                        */
+/* SAMPLE                                                                 */
+/*                                                                        */
+/* (c) Copyright IBM Corp. 2020 All Rights Reserved                       */
+/*                                                                        */
+/* US Government Users Restricted Rights - Use, duplication or disclosure */
+/* restricted by GSA ADP Schedule Contract with IBM Corp                  */
+/*                                                                        */
+
 package com.ibm.cicsdev.springboot.link.app.ui;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
