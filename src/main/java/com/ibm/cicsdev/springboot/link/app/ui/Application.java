51d76e2d945bd68f5d7702aa3c2ecc07f9fbcdc7
Alex Brown <BROWNAL7@uk.ibm.com>
2024-06-13 13:59:51 +0100

    Move to GitHub actions.

--- src/main/java/com/ibm/cicsdev/springboot/link/app/ui/Application.java
+++ src/main/java/com/ibm/cicsdev/springboot/link/app/ui/Application.java
@@ -33,13 +33,8 @@ public class Application
 	@Bean
 	public Converter<String, Message> messageConverter() 
 	{
-		return new Converter<String, Message>() 
-		{
-			@Override
-			public Message convert(String id) 
-			{
-				return messageRepository().findMessage(Long.valueOf(id));
-			}
+		return id -> {
+			return messageRepository().findMessage(Long.valueOf(id));
 		};
 	}
 
